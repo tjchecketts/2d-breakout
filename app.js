692d8322cad8cbecac972b36ffce524c650f318d
“TJ <tylerchecketts@gmail.com>
2018-03-05 14:36:15 -0700

    ball moving and add tab icon

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
+// start location of ball
+var x = canvas.width/2
+var y = canvas.height-30
 
-ctx.beginPath()
-ctx.rect(20, 40, 50, 50)
-ctx.fillStyle = "#ff0000"
-ctx.fill()
-ctx.closePath()
+// speed and direction of ball
+var dx = 2
+var dy = -2
 
-ctx.beginPath()
-ctx.arc(240, 160, 20, 0, Math.PI*2, false)
-ctx.fillStyle = "green"
-ctx.fill()
-ctx.closePath()
+// same as:
+// function drawBall() {...}
+var drawBall = () => {
+  ctx.beginPath()
+  ctx.arc(x, y, 10, 0, Math.PI*2)
+  ctx.fillStyle = "#0095dd"
+  ctx.fill()
+  ctx.closePath()
+}
 
-ctx.beginPath()
-ctx.rect(160, 10, 100, 40)
-ctx.strokeStyle = "rgba(0, 0, 255, .5)"
-ctx.stroke()
-ctx.closePath()
+var draw = () => {
+  // clears ball each frame
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
+  x += dx
+  y += dy
+}
+
+// run every 10 miliseconds
+// lower = faster frame refresh
+setInterval(draw, 10)
