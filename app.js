e115b3935610db1bc097dfda383ee18f0d32320e
“TJ <tylerchecketts@gmail.com>
2018-03-08 15:57:55 -0700

    add bricks and collider

--- app.js
+++ app.js
@@ -17,11 +17,62 @@ var paddleX = (canvas.width - paddleWidth)/2
 var rightPressed = false
 var leftPressed = false
 
+// brick variables
+var brickRowCount = 3
+var brickColumnCount = 5
+var brickWidth = 75
+var brickHeight = 20
+var brickPadding = 10
+var brickOffsetTop = 30
+var brickOffsetLeft = 30
+
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", keyUpHandler, false)
 
-// same as:
-// function drawBall() {...}
+// brick array
+var bricks = []
+for(col = 0; col < brickColumnCount; col++) {
+  bricks[col] = []
+  for(row = 0; row < brickRowCount; row++) {
+    bricks[col][row] = { x: 0, y: 0, status: 1 }
+  }
+}
+
+// draw bricks
+var drawBricks = () => {
+  for(col = 0; col < brickColumnCount; col++) {
+    for(row = 0; row < brickRowCount; row++) {
+      if(bricks[col][row].status == 1) {
+        var brickX = (col * (brickWidth + brickPadding)) + brickOffsetLeft
+        var brickY = (row * (brickHeight + brickPadding)) + brickOffsetTop
+        bricks[col][row].x = brickX
+        bricks[col][row].y = brickY
+        ctx.beginPath()
+        ctx.rect(brickX, brickY, brickWidth, brickHeight)
+        ctx.fill()
+        ctx.closePath()
+      }
+    }
+  }
+}
+
+// collision detector
+var collisionDetector = () => {
+  for(col = 0; col < brickColumnCount; col++) {
+    for(row = 0; row < brickRowCount; row++) {
+      var b = bricks[col][row]
+      if(b.status == 1) {
+        if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy
+          b.status = 0
+          randColor()
+        }
+      }
+    }
+  }
+}
+
+// draw ball
 var drawBall = () => {
   ctx.beginPath()
   ctx.arc(x, y, ballRadius, 0, Math.PI*2)
@@ -38,8 +89,10 @@ var randColor = () => {
 var draw = () => {
   // clears ball each frame
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBricks()
   drawBall()
   drawPaddle()
+  collisionDetector()
   x += dx
   y += dy
   // bounces off x & y walls
@@ -47,9 +100,18 @@ var draw = () => {
     dx = -dx
     randColor()
   }
-  if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    dy = -dy
+  if(y + dy < ballRadius) {
+    dy = -dy;
     randColor()
+  } else if(y + dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+        randColor()
+      }
+      // end game
+      else {
+        document.location.reload()
+      }
   }
   // paddle mover
   if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -60,7 +122,8 @@ var draw = () => {
   }
 }
 
-function drawPaddle() {
+// draw paddle
+var drawPaddle = () => {
   ctx.beginPath()
   ctx.rect(paddleX, canvas.height - paddelHeight, paddleWidth, paddelHeight)
   ctx.fill()
